0a85b6a687846fc3c54e31c06310a6856a70a659
frank26080115 <frank.zhao.main@gmail.com>
2026-04-12 23:21:47 -0700

    ignore upgraded lr1121

--- src/lib/LR1121Driver/LR1121.cpp
+++ src/lib/LR1121Driver/LR1121.cpp
@@ -69,8 +69,8 @@ bool LR1121Driver::Begin(uint32_t minimumFrequency, uint32_t maximumFrequency)
     DBGLN("Read LR1121 #1 Use Case (0x03 = LR1121): %d", version[2]);
     if (version[2] != 0x03)
     {
-    DBGLN("LR1121 #1 failed to be detected.");
-        return false;
+        DBGLN("LR1121 #1 failed to be detected.");
+        //return false;
     }
     DBGLN("LR1121 #1 Ready");
 
